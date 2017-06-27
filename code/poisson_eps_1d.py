f2b2945e21e6d49a6993db525550d031e0c260a5
0mar <omsrichardson@gmail.com>
2017-06-27 11:49:21 +0200

    Included exact solution and modularized

--- code/poisson_eps_1d.py
+++ code/poisson_eps_1d.py
@@ -8,40 +8,60 @@ from __future__ import print_function
 import numpy as np
 import fenics as fe
 
-# Create mesh and define function space
-mesh = fe.UnitIntervalMesh(800)
-V = fe.FunctionSpace(mesh, 'P', 1)
 
-# Define boundary condition
-u_D = fe.Constant(0.0)
+def solve_poisson_eps(h, eps, plot=False):
+    eps = fe.Constant(eps)
+    n = int(1 / h)
+    # Create mesh and define function space
+    mesh = fe.UnitIntervalMesh(n)
+    V = fe.FunctionSpace(mesh, 'P', 1)
+
+    # Define boundary condition
+    u_D = fe.Constant(0.0)
+
+    # Find exact solution:
+    u_exact = fe.Expression(
+        "(1./2 - x[0]) * (2 * x[0] + eps/(2*pi) * sin(2*pi*x[0]/eps)) "
+        "+ eps*eps/(2*pi*2*pi) * (1 - cos(2*pi*x[0]/eps)) + x[0]*x[0]",
+        eps=eps, degree=2)
+
+    def boundary(x, on_boundary):
+        return on_boundary
+
+    bc = fe.DirichletBC(V, u_D, boundary)
+
+    # Define variational problem
+
+    u = fe.TrialFunction(V)
+    v = fe.TestFunction(V)
+    f = fe.Constant(1)
+    A = fe.Expression('1./(2+cos(2*pi*x[0]/eps))', eps=eps, degree=2)
+    a = A * fe.dot(fe.grad(u), fe.grad(v)) * fe.dx
+    L = f * v * fe.dx
+
+    # Compute solution
+    u = fe.Function(V)
+    fe.solve(a == L, u, bc)
+
+    if plot:
+        # Plot solution
+        fe.plot(u)
+        fe.plot(u_exact, mesh=mesh)
+        # # Hold plot
+        fe.interactive()
+
+    # # Save solution to file in VTK format
+    # vtkfile = fe.File('poisson/solution.pvd')
+    # vtkfile << u
 
 
-def boundary(x, on_boundary):
-    return on_boundary
+    # Compute error
+    err = fe.errornorm(u_exact, u, 'L2')
+    return err
 
 
-bc = fe.DirichletBC(V, u_D, boundary)
-
-# Define variational problem
-eps = fe.Constant(1E-1)
-u = fe.TrialFunction(V)
-v = fe.TestFunction(V)
-f = fe.Constant(1)
-A = fe.Expression('1./(2+cos(2*pi*x[0]/eps))', degree=2, eps=eps)
-a = A*fe.dot(fe.grad(u), fe.grad(v)) * fe.dx
-L = f * v * fe.dx
-
-# Compute solution
-u = fe.Function(V)
-fe.solve(a == L, u, bc)
-
-# Plot solution
-fe.plot(u)
-fe.plot(A,mesh=mesh)
-
-# # Save solution to file in VTK format
-# vtkfile = fe.File('poisson/solution.pvd')
-# vtkfile << u
-
-# Hold plot
-fe.interactive()
+if __name__ == "__main__":
+    h = 1. / 800
+    eps = 1. / 10
+    err = solve_poisson_eps(h, eps, plot=False)
+    print(err)
