ae1339fad5309358ddc93b64a9d88b9f3f749f71
Omar Richardson <omsrichardson@gmail.com>
2017-08-15 16:39:44 +0200

    added image maker

---
+++ presentation/nice_image.py
@@ -0,0 +1,34 @@
+import tkinter
+import numpy as np
+
+window = tkinter.Tk()
+size = 700
+window.geometry("%dx%d" % (size,size))
+canvas = tkinter.Canvas(window,width=size,height=size)
+canvas.pack(fill=tkinter.BOTH, expand=1)
+
+min_radius = 20
+max_radius = 30
+n = 3
+tag = [0]
+def add_circle(event):
+    if tag[0] % 2== 1:
+        n = 20
+    else:
+        n = 3
+    tag[0] += 1
+    canvas.delete('all')
+    for i in range(n):
+        for j in range(n):
+            rad = size/n/3
+            loc = (size/n/2 + i*size/n, size/n/2 + j*size/n)
+            dist = (loc[0] - size/2)**2/1.6 + (loc[1] - size/2)**2
+            if dist < (size/4)**2:
+                c = np.random.randint(90,100)
+                color = "#%02x%02x%02x"%((c,)*3)
+                canvas.create_oval(loc[0] - rad,loc[1] - rad,loc[0] + rad, loc[1] + rad, fill=color)
+                canvas.create_rectangle(i*size/n,j*size/n,(i+1)*size/n,(j+1)*size/n)
+
+
+window.bind("<Button-1>",add_circle)
+window.mainloop()
